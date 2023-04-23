9294e51c98697b5e54f1c9184a17da5c19afebbf
sixwaaaay <lw.2675@qq.com>
2023-04-23 21:10:12 +0800

    feat: add toast

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,40 @@
   -->
 
 <template>
-  <router-view />
+  <router-view/>
+  <notifications position="top center"/>
 </template>
 
 <script setup lang="ts">
 
 </script>
+<style>
+.vue-notification {
+  border-radius: 4px;
+  margin: 0 5px 5px;
+  padding: 1rem;
+  /*font-size: 12px;*/
+  color: #ffffff;
+  background: #44a4fc;
+  border-left: 0 solid #187fe7;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 2rem;
+  letter-spacing: normal;
+}
+
+.vue-notification.success {
+  background: #625b71;
+  border-left-color: #42a85f;
+}
+
+.vue-notification.warn {
+  background: #f2b8b5;
+  border-left-color: #f48a06;
+}
+
+.vue-notification.error {
+  background: #b3261e;
+  border-left-color: #b82e24;
+}
+</style>
